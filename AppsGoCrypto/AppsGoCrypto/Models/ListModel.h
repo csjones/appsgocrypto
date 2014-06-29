f881514f6b02a9a23fb6e98bad227828b401e6da
Chris <crookiemoonster+github@gmail.com>
2014-06-29 14:47:49 -0400

    Added AGC List Model

---
+++ AppsGoCrypto/AppsGoCrypto/Models/ListModel.h
@@ -0,0 +1,13 @@
+//
+//  ListModel.h
+//  AppsGoCrypto
+//
+//  Created by Chris on 6/29/14.
+//  Copyright (c) 2014 GigaBitcoin, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ListModel : NSObject
+
+@end
---
+++ AppsGoCrypto/AppsGoCrypto/Models/ListModel.m
@@ -0,0 +1,13 @@
+//
+//  ListModel.m
+//  AppsGoCrypto
+//
+//  Created by Chris on 6/29/14.
+//  Copyright (c) 2014 GigaBitcoin, LLC. All rights reserved.
+//
+
+#import "ListModel.h"
+
+@implementation ListModel
+
+@end
