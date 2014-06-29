ea84e561c5b025773dbe707c5443c87a421a090b
Chris <crookiemoonster+github@gmail.com>
2014-06-29 14:47:25 -0400

    Added iTunes Search Manager

---
+++ AppsGoCrypto/AppsGoCrypto/Managers/iTunesSearchManager.h
@@ -0,0 +1,17 @@
+//
+//  iTunesSearchManager.h
+//  AppsGoCrypto
+//
+//  Created by Chris on 6/29/14.
+//  Copyright (c) 2014 GigaBitcoin, LLC. All rights reserved.
+//
+
+#import "AFHTTPRequestOperationManager.h"
+
+@interface iTunesSearchManager : AFHTTPRequestOperationManager
+
++ ( id )sharedInstance;
+
+- ( void )lookupId:( NSString* )lookupId success:( void ( ^ )( id file ) )success failure:( void ( ^ )( NSError* error ) )failure;
+
+@end
---
+++ AppsGoCrypto/AppsGoCrypto/Managers/iTunesSearchManager.m
@@ -0,0 +1,63 @@
+//
+//  iTunesSearchManager.m
+//  AppsGoCrypto
+//
+//  Created by Chris on 6/29/14.
+//  Copyright (c) 2014 GigaBitcoin, LLC. All rights reserved.
+//
+
+#import "iTunesSearchManager.h"
+
+@implementation iTunesSearchManager
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark    -   NSObject
+
+- ( id )initWithBaseURL:( NSURL* )url
+{
+    if ( self = [super initWithBaseURL:url] )
+    {
+        self.responseSerializer = [AFJSONResponseSerializer serializer];
+        
+        self.responseSerializer.acceptableContentTypes = [[NSSet alloc] initWithArray:@[ @"text/javascript" ]];
+    }
+    
+    return self;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark    -   Singleton
+
++ ( iTunesSearchManager* )sharedInstance
+{
+    static iTunesSearchManager* sharedInstance = nil;
+    
+    if(sharedInstance)
+        return sharedInstance;
+    
+    static dispatch_once_t pred;	// Lock
+    
+    dispatch_once(&pred,
+                  ^{	// This code is called at most once per app
+                      sharedInstance = [[iTunesSearchManager alloc] initWithBaseURL:[NSURL URLWithString:@"https://itunes.apple.com/"]];
+                  });
+    
+    return sharedInstance;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark    -   Public
+
+- ( void )lookupId:( NSString* )lookupId success:( void ( ^ )( id file ) )success failure:( void ( ^ )( NSError* error ) )failure
+{
+    [self GET:@"lookup"
+   parameters:@{ @"id" : lookupId }
+      success:^( AFHTTPRequestOperation* operation, id responseObject ) {
+          success( responseObject );
+      }
+      failure:^( AFHTTPRequestOperation* operation, NSError* error ) {
+          failure( error );
+      }];
+}
+
+@end
